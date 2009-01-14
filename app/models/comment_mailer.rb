a74eece58aa818cc38ceed2f3edfa61c929dd3da
micah rich <micah@micahrich.com>
2009-01-14 02:13:18 -0500

    html emails

--- app/models/comment_mailer.rb
+++ app/models/comment_mailer.rb
@@ -2,16 +2,19 @@ class CommentMailer < ActionMailer::Base
 
   def micah_notifier(comment)
     recipients  "micah@micahrich.com"
-    from        '<micah takes photos> blog@micahrich.com'
+    from        'micah takes photos < blog@micahrich.com >'
     subject     "a new comment on your blog!"
     body        :comment => comment
+    content_type 'text/html'
+    
   end
   
 
   def comment_notifier(reciever, comment)
     recipients  reciever.email
-    from        '<micah takes photos> blog@micahrich.com'
+    from        'micah takes photos < blog@micahrich.com >'
     subject     "a new comment on micah's blog!"
     body        :comment => comment
+    content_type 'text/html'
   end
 end

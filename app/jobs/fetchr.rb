a351610602dff8a4e8cd457394842378764c79fd
micah rich <micah@micahrich.com>
2009-01-16 15:17:47 -0500

    fetchr fetching photos again

--- app/jobs/fetchr.rb
+++ app/jobs/fetchr.rb
@@ -8,7 +8,7 @@ class Fetchr < ActiveRecord::Base
      @flickr ||= Fleakr.api_key = 'fb5cf2883adb61c3978689f570542433'
      @user ||= Fleakr.user('info@micahrich.com')
      
-     @photos ||= @user.sets.first
+     @photos ||= @user.sets.first.photos
      @photos.each do |photo|
        @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
        unless @post.updated_at == photo.updated_at

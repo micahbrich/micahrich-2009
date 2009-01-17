23d3f8630c1bd4c555df561132dfd92adf72e4ad
micah rich <micah@micahrich.com>
2009-01-17 15:41:52 -0500

    photoblog is oldest public photoset

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
     if admin?
        @flickr ||= Fleakr.api_key = 'fb5cf2883adb61c3978689f570542433'
        @user ||= Fleakr.user('info@micahrich.com')
-       @photos ||= @user.sets.select {|set| set.title == "photoblog"}.photos
+       @photos ||= @user.sets.last.photos
 
        @photos.each do |photo|
          @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )

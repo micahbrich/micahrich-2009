7b2859cedf93b82727e284bd11e69603ef1dc4f8
micah rich <micah@micahrich.com>
2009-01-17 15:21:03 -0500

    set selector in place

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,19 +14,20 @@ class PostsController < ApplicationController
   
   def gettr
     if admin?
-      @flickr ||= Fleakr.api_key = FLICKR_CONFIG['api_key']
-      @user ||= Fleakr.user(FLICKR_CONFIG['user_email'])
-     
-      @photos ||= @user.sets.first.photos
-      @photos.each do |photo|
-        @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
-        unless @post.updated_at == photo.updated_at
-          @post.update_attributes!(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
-        end
-      end
-      redirect_to archive_path
+       @flickr ||= Fleakr.api_key = 'fb5cf2883adb61c3978689f570542433'
+       @user ||= Fleakr.user('info@micahrich.com')
+       @set ||= @user.sets.select {|set| set.title == "photoblog"}
+       @photos || = @set.photos
+
+       @photos.each do |photo|
+         @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
+         unless @post.updated_at == photo.updated_at
+           @post.update_attributes!(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
+         end
+       end
+      redirect_to root_path
     else
-      redirect_to archive_path
+      redirect_to root_path
     end
   end
   

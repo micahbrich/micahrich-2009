3acf280213ab45f39d863248d1edfda1264044e9
micah rich <micah@micahrich.com>
2009-01-17 01:28:37 -0500

    special gettr

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,16 +13,21 @@ class PostsController < ApplicationController
   end
   
   def gettr
-    Fleakr.api_key = FLICKR_CONFIG['api_key']
-    @user = Fleakr.user(FLICKR_CONFIG['user_email'])
-    @user.sets.each do |set|
-      if set.title == FLICKR_CONFIG['photoset']
-        set.photos.each do |photo|
-          @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
+    if admin?
+      @flickr ||= Fleakr.api_key = FLICKR_CONFIG['api_key']
+      @user ||= Fleakr.user(FLICKR_CONFIG['user_email'])
+     
+      @photos ||= @user.sets.first.photos
+      @photos.each do |photo|
+        @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
+        unless @post.updated_at == photo.updated_at
+          @post.update_attributes!(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
         end
       end
+      redirect_to archive_path
+    else
+      redirect_to archive_path
     end
-    redirect_to archive_path
   end
   
   def archive

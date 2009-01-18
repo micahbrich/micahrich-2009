80d7fa4e6d19cffa65e4baca672e4a5f620c9bff
micah rich <micah@micahrich.com>
2009-01-18 05:15:47 -0500

    fetchr.rb fixed

--- app/jobs/fetchr.rb
+++ app/jobs/fetchr.rb
@@ -6,16 +6,15 @@ class Fetchr < ActiveRecord::Base
 
 
      @flickr ||= Fleakr.api_key = 'fb5cf2883adb61c3978689f570542433'
-     @user ||= Fleakr.user('info@micahrich.com')
-     @set ||= @user.sets.select {|set| set.title == "photoblog"}
-     @photos || = @set.photos
-     
-     @photos.each do |photo|
-       @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
-       unless @post.updated_at == photo.updated_at
-         @post.update_attributes!(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
-       end
-     end
+      @user ||= Fleakr.user('info@micahrich.com')
+      @photos ||= @user.sets.last.photos
+
+      @photos.each do |photo|
+        @post = Post.find_or_create_by_url(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
+        unless @post.updated_at == photo.updated_at
+          @post.update_attributes!(:url => photo.url, :title => photo.title, :description => photo.description, :created_at => photo.posted_at, :updated_at => photo.updated_at, :image => photo.large.url, :thumb => photo.small.url, :flickr_id => photo.id )
+        end
+      end
   
   # the first idea
   # @user.sets.each do |set|

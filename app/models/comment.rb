e352d217a7ca898a0d03c5af19a2b3f2a33aeb4a
micah rich <micah@micahrich.com>
2009-02-15 01:17:22 -0500

    clean url

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,10 +5,20 @@ class Comment < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :email
   validates_presence_of :body
+  before_save :clean_url
 
   def self.post
     Comment.find_by_post_id(self.post_id)
   end
 
+  def clean_url
+    unless self.website.blank?
+      unless self.website =~ /https?:\/\/.*/
+         write_attribute :website, "http://" + self.website.to_s
+      else
+         write_attribute :website, self.website
+      end
+    end
+  end
   
 end
